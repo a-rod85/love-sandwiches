a710328b2b453c0c9e1269082717b392ba2fea88
a-rod85 <31aaronanderson@gmail.com>
2022-07-12 18:40:34 +0000

    removed code from line 23 and down

--- run.py
+++ run.py
@@ -21,7 +21,3 @@ def get_sales_data():
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60")
 
-    data_str = input("Enter your data: ")
-     print(f"The data provided is{data_str}")
-
-     get_sales_data()
