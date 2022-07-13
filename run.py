b30397bc4c28c73dd5472412d51bec1bce6ba9c8
a-rod85 <31aaronanderson@gmail.com>
2022-07-13 07:14:07 +0000

    coomimi

--- run.py
+++ run.py
@@ -12,16 +12,8 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-def get_sales_data():
-    """
-    Get sales input from the user
-    """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example: 10,10,30,40,50,60")
+sales = SHEET.worksheet('sales')
 
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+data = sales.get_all_values()
 
-
-    get_sales_data()
+print()
